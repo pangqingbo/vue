77ddea2786a0eccae158670e802a5c96a54bf94a
pangqingbo <2362698454@qq.com>
2021-10-08 23:34:59 +0800

    exercise

---
+++ exercise02/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>{{arr}}</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      arr: []
+    }
+  },
+  methods: {
+    getLabel(obj) {
+      if(!obj.objChildren) return this.arr.push(obj.label)
+      this.arr.push(obj.label)
+      let tempObj = obj.objChildren
+      return this.getLabel(tempObj)
+    }
+  },
+  mounted(){
+    let obj = {
+      label: '一级',
+      objChildren: {
+        label: '二级',
+        objChildren: {
+          label: '三级',
+          objChildren: {
+            label: '四级',
+            objChildren: {
+              label: '五级'
+            }
+          }
+        }
+      }
+    }
+    this.getLabel(obj)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
