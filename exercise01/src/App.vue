cbc896baa219218d1d6dbd0097a48546e644fbf7
pangqingbo <2362698454@qq.com>
2021-10-08 21:13:32 +0800

    exercise

--- exercise01/src/App.vue
+++ exercise01/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- ArrList循环显示100个数组对象 -->
     <ArrList />
   </div>
 </template>
@@ -11,6 +12,17 @@ export default {
   name: 'App',
   components: {
     ArrList
+  },
+  data(){
+    // 求最大最小值的数组
+    return {arr: [1,3,6,2,7]}
+  },
+  created() {
+    // 求arr的最大最小值，先排序后输出
+    this.arr.sort((value1, value2) => {
+      return value1-value2
+    })
+    console.log("Max:"+this.arr[0]+",Min:"+this.arr[this.arr.length-1])
   }
 }
 </script>
--- exercise01/src/components/ArrList.vue
+++ exercise01/src/components/ArrList.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
-        <!-- <div v-for="(item, i) in items" :key="i"> -->
-            
-        <!-- </div> -->
-        
+        <p v-for="(item, i) in items" :key="i">
+            {{item.num}}-->{{item.className}}
+        </p>
     </div>
 </template>
 
@@ -12,29 +11,29 @@ export default {
     data() {
         return {
             items: [],
-            flag: 100,
-            className: ['food', 'animal', 'apple', 'banana', 'orange', 'human', 'color', 'fish', 'video', 'drink']
+            className: [
+                'food',
+                'animal',
+                'apple',
+                'banana',
+                'orange',
+                'human',
+                'color',
+                'fish',
+                'video',
+                'drink'
+            ]
         }
     },
     created() {
-        /* let arr = Array.from(this.items)
-        let flag = 0
-        let className = this.className[flag]
-        for(let i=1; i<=this.flag; i++){
-            arr.push({num: i, class: className})
+        let flag = 100
+        let x = 0
+        for(let i=1; i<=flag; i++){
+            this.items.push({num: i, className: this.className[x]})
             if(i%10 == 0){
-                className = this.className[++flag]
+                x++
             }
         }
-        this.items = arr */
-        this.items.length = 100
-        let arr = Array.from(this.items)
-        arr.fill({})
-        arr.forEach((item,i) => {
-            item.num = i
-            console.log(i )
-        })
-        console.log(arr)
     }
 }
 </script>
