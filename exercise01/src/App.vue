1a8cc322bc0d1b2ff675cbbd98e524a45cab7e26
pangqingbo <2362698454@qq.com>
2021-10-08 18:40:56 +0800

    exercise

---
+++ exercise01/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ArrList />
+  </div>
+</template>
+
+<script>
+import ArrList from "./components/ArrList.vue"
+
+export default {
+  name: 'App',
+  components: {
+    ArrList
+  }
+}
+</script>
+
+
---
+++ exercise01/src/components/ArrList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <!-- <div v-for="(item, i) in items" :key="i"> -->
+            
+        <!-- </div> -->
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [],
+            flag: 100,
+            className: ['food', 'animal', 'apple', 'banana', 'orange', 'human', 'color', 'fish', 'video', 'drink']
+        }
+    },
+    created() {
+        /* let arr = Array.from(this.items)
+        let flag = 0
+        let className = this.className[flag]
+        for(let i=1; i<=this.flag; i++){
+            arr.push({num: i, class: className})
+            if(i%10 == 0){
+                className = this.className[++flag]
+            }
+        }
+        this.items = arr */
+        this.items.length = 100
+        let arr = Array.from(this.items)
+        arr.fill({})
+        arr.forEach((item,i) => {
+            item.num = i
+            console.log(i )
+        })
+        console.log(arr)
+    }
+}
+</script>
